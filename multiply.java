d3116e345ec3210e04fab94e6e6f36c1d2253a81
Yash Seth <yashseth20030712@gmail.com>
2023-10-14 17:24:30 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+
+    public static void main (String args[]){
+        int a=5 ; 
+        int b= 6 ; 
+        System.out.println("the multiply is "+a*b);
+       
+
+    }
+    
+}
